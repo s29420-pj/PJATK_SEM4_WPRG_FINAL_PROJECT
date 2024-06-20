1e190faf512e479417b750b05c17689c8e414f15
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-06-20 23:48:47 +0200

    Removed files.

--- app/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once './config.php';
-
-$connection = new mysqli(DB_NAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
