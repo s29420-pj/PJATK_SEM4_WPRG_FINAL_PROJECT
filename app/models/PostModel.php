c30dcabff5f48124fc7f385d567d1bb8f8131fe6
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-06-20 22:31:03 +0200

    Added constructors, getters and setters to models.

--- app/models/PostModel.php
+++ app/models/PostModel.php
@@ -9,4 +9,119 @@ class PostModel {
     private $image;
     private $date;
     private $authorId;
+
+    /**
+     * @param $id
+     * @param $title
+     * @param $content
+     * @param $image
+     * @param $date
+     * @param $authorId
+     */
+    public function __construct($id, $title, $content, $image, $date, $authorId)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->content = $content;
+        $this->image = $image;
+        $this->date = $date;
+        $this->authorId = $authorId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthorId()
+    {
+        return $this->authorId;
+    }
+
+    /**
+     * @param mixed $authorId
+     */
+    public function setAuthorId($authorId)
+    {
+        $this->authorId = $authorId;
+    }
+
 }
